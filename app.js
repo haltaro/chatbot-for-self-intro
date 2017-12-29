2a971e7e2240b9b4f15dda969400bc5b2652d78f
haltaro <r.yasunaga123@gmail.com>
2017-12-30 06:48:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 (function() {
 
-  var url = 'https://api.github.com/search/repositories?q=';
   var msgIndex, key;
   var botui = new BotUI('search-repo');
 
-
-  //初期メッセージ
-  botui.message.bot({
+  //初期メッセージ．
+  botui.message.add({
     content: 'はじめまして！haltaroと申します！'
   }).then(excuse);
 
-  // 社交辞令
+  // 社交辞令的なものを表示する関数．
   function excuse() {
-    botui.message.bot({
+    botui.message.add({
       delay: 1000,
       content: '本来は直接挨拶すべきところを，チャットボットで失礼致します．'
     }).then(init);
   }
 
+  // 最初に，（見かけ上）質問を自由に入力してもらう関数．
+  // 実際はちゃんと回答する気はない．
   function init() {
-    botui.message.bot({
-      delay: 1000,  //メッセージの表示タイミングをずらす
-      content: '私について，何をお話ししましょうか？'
+    botui.message.add({
+      delay: 1000,  //メッセージの表示タイミングをずらす．
+      content: '何について，お話ししましょうか？'
     }).then(function() {
 
       //キーワードの入力
-      //「return」を記述して、ユーザーからの入力待ち状態にする
       return botui.action.text({
         delay: 1000,
         action: {
-          placeholder: '質問を入力してください．'
+          placeholder: '質問を入力'
         }
       });
 
     }).then(function(res){
 
       key = res.value;
-      return botui.message.bot({
+      return botui.message.add({
         delay: 1000,
         content: '「' + key + '」…？'
       });
@@ -47,160 +46,139 @@
 
   // よくわからない質問を適当にあしらう関数．
   function tomizawa() {
-    botui.message.bot({
+    botui.message.add({
       delay: 2500,
       content: 'ちょっと何言ってるかわからないです．'
-    }).then(showOptions);
+    }).then(function() {
+      return botui.message.add({
+        delay: 1000,
+        content: 'ちなみに，好きな芸人はサンドウィッチマンです．'
+      })
+    }).then(showQuestions);
   }
 
-  // 選択肢を提示する関数．
-  function showOptions() {
-    botui.message.bot({
+  // 質問の選択肢を提示する関数．
+  function showQuestions() {
+    botui.message.add({
       delay:1000,
-      content: '次の中から，質問を選んでください．'
+      content: '質問を選んでください．'
     }).then(function() {
 
-      // 質問事項のボタンを提示
+      // ボタンを提示する．
       return botui.action.button({
         autoHide: false,
         delay: 1000,
         action: [
           {icon: 'user-o', text: '来歴', value: 'carrier'},
           {icon: 'rocket', text: '趣味', value: 'hobby'},
-          {icon: 'pencil', text: '言語', value: 'lang'},
-          {icon: 'thumbs-down',text: '興味ない', value: 'none'}]
+          {icon: 'pencil', text: 'このサイト', value: 'memotaro'}]
       });
     }).then(function(res) {
       botui.action.hide();
       switch (res.value) {
         case 'carrier': showCarrier(); break;
         case 'hobby': showHobby(); break;
+        case 'memotaro': showMemotaro(); break;
         default: end();
       }
     });
   }
 
+  // 来歴について説明する関数．
   function showCarrier() {
-    botui.message.bot({
+    botui.message.add({
       delay: 1000,
-      content: '2013年3月に，工学修士（航空宇宙工学）を修了しました．ここではGPS衛星の異常検知について研究しました．'
+      content: '大学院では，工学修士（航空宇宙工学）を修了しました．主に，GPS衛星の異常検知について研究しました．'
     }).then(function() {
-      return botui.message.bot({
-        delay: 3000,
-        content: '2013年4月に，通信会社の研究所に入社しました．主に，通信制御アルゴリズムについて研究しました．'
+      return botui.message.add({
+        delay: 2500,
+        content: '新卒で約5年間，通信会社の研究所で働きました．主に，通信制御アルゴリズムについて研究しました．'
       });
     }).then(function() {
-      return botui.message.bot({
-        delay: 3000,
-        content: '2016年9月から一年間，アメリカの外資系研究所に出向しました．これまでの研究生活で，最も幸せな一年間でした．'
+      return botui.message.add({
+        delay: 2500,
+        content: '最後の1年間は，アメリカの外資系研究所に出向しました．これまでの研究生活で，最も幸せな一年間でした．'
       });
     }).then(function() {
-      return botui.message.bot({
-        delay: 3000,
-        content: '2018年1月から，広告代理店に転職しました．データマイニングを行う予定です．'
+      return botui.message.add({
+        delay: 2500,
+        content: '現在は，広告代理店でデータマイニングやってます．'
       });
-    }).then(showOptions);
+    }).then(askEnd);
   }
 
+  // 趣味について説明する関数．
   function showHobby() {
-    botui.message.bot({
+    botui.message.add({
       delay: 1000,
-      content: '趣味はジョギングです．近所に多摩川があるので，よく走ってます．'
-    }).then(showOptions);
-  }
-
-
-/*
-  function init() {
-    botui.message.bot({
-      delay: 1500,  //メッセージの表示タイミングをずらす
-      content: '私について，何をお話したらよいでしょうか？'
+      content: '趣味はジョギングです．今年の目標は，ハーフマラソンに出場することです．'
     }).then(function() {
-
-      //キーワードの入力
-      //「return」を記述して、ユーザーからの入力待ち状態にする
-      return botui.action.text({
-        delay: 1000,
-        action: {
-          placeholder: '例：年齢'
-        }
+      return botui.message.add({
+        delay: 2000,
+        content: 'また，週末プログラマとして，c++，Python，JavaScriptなどで遊んでいます．'
       });
-    }).then(function(res) {
-
-      //入力されたキーワードを取得する
-      key = res.value;
-      getRepositories(key);
-
-      //ローディング中のアイコンを表示
-      botui.message.bot({
-        loading: true
-      }).then(function(index) {
-
-        //ローディングアイコンのindexを取得
-        //このindexを使ってメッセージ情報を更新する
-        //（更新しないとローディングアイコンが消えないため…）
-        msgIndex = index;
+    }).then(function() {
+      return botui.message.add({
+        delay: 2500,
+        content: 'Network engineeringとか，Algorithmic tradingとか，Machine learningに興味があります．詳細はProjectsをご参照ください．'
       });
-    });
+    }).then(askEnd);
   }
 
-
-  //GitHubのリポジトリを取得する処理
-  function getRepositories(keyword) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', url + keyword);
-    xhr.onload = function() {
-      var result = JSON.parse(xhr.responseText);
-
-      //取得したリポジトリ数をshowMessage()に代入する
-      showMessage(result.total_count);
-    }
-    xhr.send();
+  // 本ウェブサイトについて説明する関数．
+  function showMemotaro() {
+    botui.message.add({
+      delay: 1000,
+      content: 'このウェブサイトには，Qiita以下で日記以上な技術的メモを書く予定です．'
+    }).then(function(){
+      return botui.message.add({
+        delay: 2000,
+        content: '想定読者は私自身です．'
+      });
+    }).then(function(){
+      return botui.message.add({
+        delay: 2000,
+        content: 'Categoryには，各記事をカテゴリごとにまとめています．'
+      });
+    }).then(function(){
+      return botui.message.add({
+        delay: 2000,
+        content: 'Projectsには，これまで作成したプロジェクトをまとめています．'
+      });
+    }).then(function(){
+      return botui.message.add({
+        delay: 2000,
+        content: 'なお，本ウェブサイトの内容を，予告なく変更・削除する場合2があります．何卒ご理解のほど，よろしくお願い致します．'
+      });
+    }).then(askEnd);
   }
-*/
 
-  //リポジトリ総数をメッセージに表示する処理
-  function showMessage(totalCount) {
-
-    //ローディングアイコンのindexを使ってメッセージを書き換える
-    botui.message.update(msgIndex, {
-      content: key + 'のリポジトリ総数は、' + totalCount + '個です！'
-    }).then(function() {
-      return botui.message.bot({
-        delay: 1500,
-        content: 'まだ続けますか？'
-      })
+  // プログラムを終了するか聞く関数．
+  function askEnd(){
+    botui.message.add({
+      delay:2000,
+      content: 'まだ続けますか？'
     }).then(function() {
 
-      //「はい」「いいえ」のボタンを表示
+      // ボタンを提示する．
       return botui.action.button({
         delay: 1000,
-        action: [{
-          icon: 'circle-thin',
-          text: 'はい',
-          value: true
-        }, {
-          icon: 'close',
-          text: 'いいえ',
-          value: false
-        }]
+        action: [
+          {icon: 'circle-o', text: 'はい', value: true},
+          {icon: 'close', text: 'いいえ', value: false}]
       });
     }).then(function(res) {
-
-      //「続ける」か「終了」するかの条件分岐処理
-      res.value ? init() : end();
-    });
+      res.value ? showQuestions() : end();
+      });
   }
 
-
-  //プログラムを終了する処理
+  //プログラムを終了する関数．
   function end() {
-    botui.message.bot({
+    botui.message.add({
       delay: 1000,
       content: 'お時間頂き，ありがとうございました．'
     }).then(function(){
-      return botui.message.bot({
+      return botui.message.add({
         delay: 1000,
         content: 'それでは，引き続きよろしくお願いします！'
       });
