5b35ae9b394373eeecb7c7ef4b62ee02e51cef35
haltaro <mail.to.haltaro@gmail.com>
2017-12-30 17:28:11 +0900

    Revise scripts.

--- bot.js
+++ bot.js
@@ -1,11 +1,26 @@
 (function() {
 
-  var msgIndex, key;
+  var key;
   var botui = new BotUI('self-intro');
 
   //初期メッセージ．
   botui.message.add({
-    content: 'こんにちは！haltaroです！'
+    content: 'はじめまして，xxxと申します．'
+  }).then(function() {
+    return botui.message.add({
+      delay: 1500,
+      content: '2018年1月より，中途入社致しました．よろしくお願いします．'
+    });
+  }).then(function() {
+    return botui.message.add({
+      delay: 2000,
+      content: '本来であれば，直接伺ってご挨拶すべきところですが，それはちょっと大変ですので，'
+    });
+  }).then(function() {
+    return botui.message.add({
+      delay: 3000,
+      content: '雑に作ったチャットボットで自己紹介させて頂きます．'
+    });
   }).then(init);
 
 
@@ -13,8 +28,8 @@
   // 実際はちゃんと回答する気はない．
   function init() {
     botui.message.add({
-      delay: 1000,  //メッセージの表示タイミングをずらす．
-      content: '何について，お話ししましょうか？'
+      delay: 2000,  //メッセージの表示タイミングをずらす．
+      content: '質問を入力してください．'
     }).then(function() {
 
       //キーワードの入力
@@ -44,7 +59,7 @@
       content: 'ちょっと何言ってるかわからないです．'
     }).then(function() {
       return botui.message.add({
-        delay: 1000,
+        delay: 1500,
         content: 'ちなみに，好きな芸人はサンドウィッチマンです．'
       })
     }).then(showQuestions);
@@ -53,7 +68,7 @@
   // 質問の選択肢を提示する関数．
   function showQuestions() {
     botui.message.add({
-      delay:1000,
+      delay:1500,
       content: '質問を選んでください．'
     }).then(function() {
 
@@ -64,14 +79,14 @@
         action: [
           {icon: 'user-o', text: '来歴', value: 'carrier'},
           {icon: 'rocket', text: '趣味', value: 'hobby'},
-          {icon: 'pencil', text: 'このサイト', value: 'memotaro'}]
+          {icon: 'comment', text: '意気込み', value: 'comment'}]
       });
     }).then(function(res) {
       botui.action.hide();
       switch (res.value) {
         case 'carrier': showCarrier(); break;
         case 'hobby': showHobby(); break;
-        case 'memotaro': showMemotaro(); break;
+        case 'comment': showComment(); break;
         default: end();
       }
     });
@@ -81,21 +96,33 @@
   function showCarrier() {
     botui.message.add({
       delay: 1000,
-      content: '大学院では，工学修士（航空宇宙工学）を修了しました．主に，GPS衛星の異常検知について研究しました．'
+      content: 'xxx出身です．'
     }).then(function() {
       return botui.message.add({
-        delay: 2500,
-        content: '新卒で約5年間，通信会社の研究所で働きました．主に，通信制御アルゴリズムについて研究しました．'
+        delay: 1500,
+        content: '中学・高校は，軟式テニス部に入部しました．テニスの王子様の影響です．'
+      });
+    }).then(function() {
+      return botui.message.add({
+        delay: 1500,
+        type: 'embed',
+        content: 'fig/ryoma.jpg'
       });
     }).then(function() {
       return botui.message.add({
         delay: 2500,
-        content: '最後の1年間は，アメリカの外資系研究所に出向しました．これまでの研究生活で，最も幸せな一年間でした．'
+        content: '大学時代は，航空宇宙工学科に所属していました．宇宙兄弟の影響です．'
       });
     }).then(function() {
       return botui.message.add({
-        delay: 2500,
-        content: '現在は，広告代理店でデータマイニングやってます．'
+        delay: 1500,
+        type: 'embed',
+        content: 'https://www.youtube.com/embed/HMm725pwSp8'
+      });
+    }).then(function() {
+      return botui.message.add({
+        delay: 3000,
+        content: '新卒では，某通信会社に就職しました．xxxの影響です．'
       });
     }).then(askEnd);
   }
@@ -104,45 +131,32 @@
   function showHobby() {
     botui.message.add({
       delay: 1000,
-      content: '趣味はジョギングです．今年の目標は，ハーフマラソンに出場することです．'
+      content: '趣味はジョギングです．'
     }).then(function() {
       return botui.message.add({
-        delay: 2000,
-        content: 'また，日曜プログラマとして，c++，Python，JavaScriptなどで遊んでいます．'
+        delay: 1000,
+        type: 'embed',
+        content: 'fig/running.png'
       });
     }).then(function() {
       return botui.message.add({
-        delay: 2500,
-        content: 'Network engineeringとか，Algorithmic tradingとか，Machine learningに興味があります．詳細はProjectsをご参照ください．'
+        delay: 1500,
+        content: 'あと自転車も好きです．'
+      });
+    }).then(function() {
+      return botui.message.add({
+        delay: 1000,
+        type: 'embed',
+        content: 'fig/bicycle.png'
       });
     }).then(askEnd);
   }
 
   // 本ウェブサイトについて説明する関数．
-  function showMemotaro() {
+  function showComment() {
     botui.message.add({
       delay: 1000,
-      content: 'このウェブサイトには，Qiita以下かつ日記以上な技術的メモを書く予定です．'
-    }).then(function(){
-      return botui.message.add({
-        delay: 2000,
-        content: '想定読者は私自身です．'
-      });
-    }).then(function(){
-      return botui.message.add({
-        delay: 2000,
-        content: 'Categoryには，各記事をカテゴリごとにまとめています．'
-      });
-    }).then(function(){
-      return botui.message.add({
-        delay: 2000,
-        content: 'Projectsには，これまで作成したプロジェクトをまとめています．'
-      });
-    }).then(function(){
-      return botui.message.add({
-        delay: 2000,
-        content: 'なお，本ウェブサイトの内容を，予告なく変更・削除する場合があります．何卒ご理解のほど，よろしくお願い致します．'
-      });
+      content: 'がんばります'
     }).then(askEnd);
   }
 
@@ -150,7 +164,7 @@
   function askEnd(){
     botui.message.add({
       delay:2000,
-      content: 'まだ続けますか？'
+      content: '他に質問がありますか？'
     }).then(function() {
 
       // ボタンを提示する．
@@ -173,7 +187,7 @@
     }).then(function(){
       return botui.message.add({
         delay: 1000,
-        content: 'それでは，引き続きよろしくお願いします！'
+        content: 'それでは，よろしくお願い致します．'
       });
     });
   }
